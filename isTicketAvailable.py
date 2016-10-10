797df79ee5c4994a3fc7b3ed4ffca79e72b9b89a
Tamal Sen <pleasant.art.mun@gmail.com>
2016-10-10 16:25:44 +0600

    Update isTicketAvailable.py

--- isTicketAvailable.py
+++ isTicketAvailable.py
@@ -1,16 +1,16 @@
 __author__ = 'tamal.2k14@gmail.com (Tamal Sen)'
-import mechanize //Browser for python
-import lxml.html //Sorting elements of html
-import  lxml.etree //Same
-import winsound //To play beep sound
-import colorama //To colorize output
+import mechanize #Browser for python
+import lxml.html #Sorting elements of html
+import  lxml.etree #Same
+import winsound #To play beep sound
+import colorama #To colorize output
 from datetime import datetime
 import time
 
 colorama.init(strip=False, autoreset=True)
 TicketPaisi = False
-abort_after = 10 //Interval time for checking
-url = 'https://www.shohoz.com/events/bangladesh-vs-england-series-2016/book-now/2016-10-12' // url of ticket selling event
+abort_after = 10 #Interval time for checking
+url = 'https://www.shohoz.com/events/bangladesh-vs-england-series-2016/book-now/2016-10-12' # url of ticket selling event
 
 print ("---Initiating Program---")
 while (TicketPaisi== False):
@@ -23,14 +23,14 @@ while (TicketPaisi== False):
     tree = lxml.html.fromstring(content)
     info = tree.xpath('//select[@id="ticket_type"]/option')
     all_tickets = [option.text for option in info]
-    all_tickets.remove('Select Ticket Category') //unnecessary option value
+    all_tickets.remove('Select Ticket Category') #unnecessary option value
 
 #for seats in all_tickets:
 #    print seats
 
     available_tickets = [s for s in all_tickets if "Sold Out" not in s]
     print "Avaible Tickets Category: %d" % len(available_tickets)
-    if len(available_tickets) > 0: //if tickets are available play beep sound
+    if len(available_tickets) > 0: #if tickets are available play beep sound
         Freq = 3000  # Set Frequency To 2500 Hertz
         Dur = 3000  # Set Duration To 1000 ms == 1 second
         winsound.Beep(Freq, Dur)
